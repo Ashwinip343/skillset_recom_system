d089d273ba9752ca8e9e2a56972f8f07f9f8e00a
Ashwini Patil <91271985+Ashwinip343@users.noreply.github.com>
2024-01-04 22:09:53 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import PaLM from "palm-api";
 
-let bot = new PaLM('AIzaSyDEPYNJqvyccJQ1AuXAxPfyx5Ic7IafQAs');
+let bot = new PaLM('YOUR_API_KEY');
 
 bot.generateText("Write a poem on puppies.", {
 	temperature: 0.5,
